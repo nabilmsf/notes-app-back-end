973378d4e7ea25eb3af3a2dc29ea683b47921c37
nabilmsf <nabilazharm@gmail.com>
2021-07-16 22:17:47 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
-const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const Hapi = require("@hapi/hapi");
+const routes = require("./routes");
 
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: 'localhost',
+    host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
     routes: {
       cors: {
-        origin: ['*'],
-      }
-    }
+        origin: ["*"],
+      },
+    },
   });
 
   server.route(routes);
@@ -17,6 +17,5 @@ const init = async () => {
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
-
 
 init();
